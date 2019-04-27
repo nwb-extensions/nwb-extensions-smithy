b91f6069bce46fffd75663bef7d7282719a9e3fd
Ryan Ly <rly@lbl.gov>
2019-04-26 19:09:49 -0700

    Clean up compat for python 2

--- nwb_extensions_smithy/github.py
+++ nwb_extensions_smithy/github.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, print_function
-
 import os
 import os.path
 from random import choice
--- nwb_extensions_smithy/lint_recipe.py
+++ nwb_extensions_smithy/lint_recipe.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
 from collections.abc import Sequence, Mapping
 import copy
 import io
